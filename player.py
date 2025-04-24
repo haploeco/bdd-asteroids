2b698cc95ed90139c245ddb5442fedd90129b63d
Nate Thompson <haplo@haplolabs.io>
2025-04-24 05:53:15 -0600

    Removed old debug print statement

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-import pygame
+= pygame
 
 from circleshape import CircleShape
 from constants import *
@@ -27,7 +27,6 @@ class Player(CircleShape):
 
     def update(self, dt):
         self.cooldown -= dt
-        print(self.cooldown)
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
